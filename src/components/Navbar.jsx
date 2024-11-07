513b1b1c34fac94c788239b7517a296221250c5a
Mesogboriwon Daniel <dhanel0u0@gmail.com>
2024-11-07 11:05:09 -0800

    added css

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './index.css'
+
+const Navbar = () => {
+  return (
+    <>
+      <ul className='navbar'>
+        <li>OMABLOGS</li>
+        <li>Home</li>
+        <li>Add Blog</li>
+      </ul>
+    </>
+  )
+}
+
+export default Navbar
