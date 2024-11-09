93911afef3fd7b00c88acb8b13bd387f31088f52
Mesogboriwon Daniel <dhanel0u0@gmail.com>
2024-11-09 10:47:52 -0800

    added loading message

--- src/components/Bloglistings.jsx
+++ src/components/Bloglistings.jsx
@@ -4,7 +4,7 @@ const Bloglistings = ({ blogs, title, handleDelete }) => {
   return (
     <>
        <h3 className='blogtitle'>Latest Feeds:</h3>
-      {blogs.map(blog =>(
+       {blogs.map(blog =>(
         <div className='blog-preview' key={ blog.id }>
             <h3> { blog.title }</h3>
             <h5>written by: { blog.author }</h5>
