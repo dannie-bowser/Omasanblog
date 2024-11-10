106bb8b66e694b64f65458ce69fb8e4d5ad73176
Mesogboriwon Daniel <dhanel0u0@gmail.com>
2024-11-10 15:30:54 -0800

    added blogdetails page

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min'
+import useFetch from '../UseFetch';
 
 const BlogDetails = () => {
 
     const { id } = useParams();
+    const { data: blog, loading, error } = useFetch('http://localhost:8000/blogs/' + id);
+
+    console.log(blog);
 
   return (
     <div className='content'>
-        <div>BlogDetails - { id }</div>
+        {loading && <p className='loading'>Loading resources...</p>}
+        {error && <div className='loading'>{ error }</div>}
+        {blog && (
+          <article className='blog-details'>
+            <h2>{ blog.title }</h2>
+            <p>Written by: { blog.author }</p>
+            <div>{ blog.body }</div>
+          </article>
+
+        )}
     </div>
   )
 }
