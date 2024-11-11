b4c00afc6c6dcd0365b78ac8666d0070e360e83c
Mesogboriwon Daniel <dhanel0u0@gmail.com>
2024-11-10 16:29:13 -0800

    finished with form submit

--- src/components/AddBlog.jsx
+++ src/components/AddBlog.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddBlog = () => {
+
+   const [title, setTitle] = useState(null);
+   const [body, setBody] = useState(null);
+   const [author, setAuthor] = useState('Albert thorn');
+   const [loading, setLoading] = useState(false)
+
+  const handleSubmit = (e) => {
+     e.preventDefault();
+
+     const blog = { title, body, author };
+
+     setLoading(true);
+    
+      fetch('http://localhost:8000/blogs', {
+        method: 'POST',
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(blog)
+      })
+      .then(() => {
+        setLoading(false);
+      })
+  }
+
+
+
+
   return (
     <div  className='content'>
-        <div>AddBlog</div>
+        <h3 className='formhead'>Add a new blog</h3>
+        <form onSubmit={handleSubmit}>
+          <label>Blog title:</label>
+          <input
+           type="text"
+           required
+           value={title}
+           onChange={(e) => setTitle(e.target.value)}
+          />
+          <label>Blog body:</label>
+          <textarea
+           required
+           value={body}
+           onChange={(e) => setBody(e.target.value)}
+          />
+          <label>Blog title:</label>
+          <select
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          >
+            <option value="Albert thorn">Albert thorn</option>
+            <option value="Beneke johnson">Beneke johnson</option>
+            <option value="Josephine O'reily">Josephine O'reily</option>
+          </select>
+          {!loading && <button>Publish blog</button>}
+          {loading && <button disabled>Publishing blog...</button>}
+        </form>
     </div>
   )
 }
